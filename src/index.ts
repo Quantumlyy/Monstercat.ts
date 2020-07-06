8b1def5a18fe9a179f38686f5bf9029692acc34c
QuantumlyTangled <ethoarx@gmail.com>
2020-07-06 13:53:15 +0200

    Ignore

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { Monstercat } from './lib/Monstercat';
 export * from './lib/structures/ApiHandler';
 export { CookieJar } from './lib/util/CookieJar';
 
+export * from './lib/structures/data/Common';
 export * from './lib/structures/data/SelfPlaylists';
 export * from './lib/structures/data/PlaylistPid';
+export * from './lib/structures/data/PlaylistPidCatalog';
