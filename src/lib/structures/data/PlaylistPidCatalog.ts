acd160a8f433eaef3599bc7b162fa12556283a01
QuantumlyTangled <ethoarx@gmail.com>
2020-07-07 18:45:25 +0200

    huh

--- src/lib/structures/data/PlaylistPidCatalog.ts
+++ src/lib/structures/data/PlaylistPidCatalog.ts
@@ -106,3 +106,57 @@ export class PlaylistPidCatalogRelease extends DataRawBase<IPlaylistPidCatalogRe
 	}
 
 }
+
+export class PlaylistPidCatalogResult extends DataRawBase<IPlaylistPidCatalogResult> implements IPlaylistPidCatalogResult {
+
+	public readonly artistsTitle: string;
+	public readonly bpm: number;
+	public readonly creatorFriendly: boolean;
+	public readonly debutDate: Date;
+	public readonly downloadable: boolean;
+	public readonly duration: number;
+	public readonly explicit: boolean;
+	public readonly genrePrimary: string;
+	public readonly genreSecondary: string;
+	public readonly id: string;
+	public readonly inEarlyAccess: boolean;
+	public readonly isrc: string;
+	public readonly streamable: boolean;
+	public readonly title: string;
+	public readonly trackNumber: number;
+	public readonly tags: string[];
+	public readonly version: string;
+	public readonly release: IPlaylistPidCatalogRelease;
+	public readonly artists: IPlaylistPidCatalogArtist[];
+	public readonly playlistSort: number;
+
+	protected constructor(data: IPlaylistPidCatalogResult) {
+		super(data);
+
+		this.artistsTitle = String(this.data.artistsTitle);
+		this.bpm = Number(this.data.bpm);
+		this.creatorFriendly = Boolean(this.data.creatorFriendly);
+		this.debutDate = new Date(this.data.debutDate);
+		this.downloadable = Boolean(this.data.downloadable);
+		this.duration = Number(this.data.duration);
+		this.explicit = Boolean(this.data.explicit);
+		this.genrePrimary = String(this.data.genrePrimary);
+		this.genreSecondary = String(this.data.genreSecondary);
+		this.id = String(this.data.id);
+		this.inEarlyAccess = Boolean(this.data.inEarlyAccess);
+		this.isrc = String(this.data.isrc);
+		this.streamable = Boolean(this.data.streamable);
+		this.title = String(this.data.title);
+		this.trackNumber = Number(this.data.trackNumber);
+		this.tags = this.data.tags.map(tag => String(tag));
+		this.version = String(this.data.version);
+		this.release = PlaylistPidCatalogRelease.create(this.data.release);
+		this.artists = this.data.artists.map(artist => PlaylistPidCatalogArtist.create(artist));
+		this.playlistSort = Number(this.data.playlistSort);
+	}
+
+	public static create(data: IPlaylistPidCatalogResult): PlaylistPidCatalogResult {
+		return new PlaylistPidCatalogResult(data);
+	}
+
+}
